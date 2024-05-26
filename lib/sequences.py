25659fa46fe87eece7ed877b899eb63a4762782e
BiggerH2 <161705584+BiggerH2@users.noreply.github.com>
2024-05-26 22:32:47 +0300

    updated

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        return []
+    elif length == 1:
+        return [0]
+    else:
+        fib_sequence = [0, 1]
+        while len(fib_sequence) < length:
+            next_fib = fib_sequence[-1] + fib_sequence[-2]
+            fib_sequence.append(next_fib)
+        return fib_sequence
--- lib/testing/lib_test.py
+++ lib/testing/lib_test.py
@@ -1,42 +1,23 @@
 #!/usr/bin/env python3
 
-from sequences import print_fibonacci
-
 import io
 import sys
+import pytest
+from sequences import print_fibonacci
 
 
 class TestPrintFibonacci:
-    '''function print_fibonacci()'''
+    '''Test cases for print_fibonacci function'''
 
-    def test_print_fibonacci_zero(self):
-        '''prints empty list when length = 0'''
+    @pytest.mark.parametrize("length, expected", [
+        (0, []),
+        (1, [0]),
+        (2, [0, 1]),
+        (10, [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]),
+    ])
+    def test_print_fibonacci(self, length, expected):
         captured_out = io.StringIO()
         sys.stdout = captured_out
-        print_fibonacci(0)
+        result = print_fibonacci(length)
         sys.stdout = sys.__stdout__
-        assert(captured_out.getvalue() == '[]\n')
-
-    def test_print_fibonacci_one(self):
-        '''prints 0 when length = 1'''
-        captured_out = io.StringIO()
-        sys.stdout = captured_out
-        print_fibonacci(1)
-        sys.stdout = sys.__stdout__
-        assert(captured_out.getvalue() == '[0]\n')
-
-    def test_print_fibonacci_two(self):
-        '''prints 0\\n1 when length = 2'''
-        captured_out = io.StringIO()
-        sys.stdout = captured_out
-        print_fibonacci(2)
-        sys.stdout = sys.__stdout__
-        assert(captured_out.getvalue() == '[0, 1]\n')
-
-    def test_print_fibonacci_ten(self):
-        '''prints 0\\n1\\n1\\n2\\n3\\n5\\n8\\n13\\n21\\n34 when length = 10'''
-        captured_out = io.StringIO()
-        sys.stdout = captured_out
-        print_fibonacci(10)
-        sys.stdout = sys.__stdout__
-        assert(captured_out.getvalue() == '[0, 1, 1, 2, 3, 5, 8, 13, 21, 34]\n')
+        assert result == expected
